b99bb6e754318966cf032741c260d29a1148c93e
Pedro Henrique Pereira de Souza <pedro.henrique.filomena@gmail.com>
2025-11-16 14:42:59 +0000

    <script type="module">   // Import the functions you need from the SDKs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,10 @@
 // You can find this in your Firebase project settings.
 // This is placeholder data and will not work.
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "1:your-sender-id:web:your-app-id",
-  measurementId: "G-XXXXXXXXXX"
+    apiKey: "AIzaSyDZDskmvrPJPF-fAwxmZ046BHkfnPcLO2M",
+    authDomain: "pixel-emporium-homol.firebaseapp.com",
+    projectId: "pixel-emporium-homol",
+    storageBucket: "pixel-emporium-homol.appspot.com",
+    messagingSenderId: "878528460092",
+    appId: "1:878528460092:web:0db34ab91c0bb5c6d9da39"
 };
